29bb556b569de308a3b782ae60cd0516cb561946
sachinsudhesan <sachinchandra276@gmail.com>
2024-03-07 20:41:52 +0530

    hello world

--- basichelloworld/index.js
+++ basichelloworld/index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT 
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+app.get('/sachin',(req,res) => {
+    res.send('hello sachin')
+})
+
+app.listen(port,() => {
+    console.log((`app listening on port:${port}`));
+})
